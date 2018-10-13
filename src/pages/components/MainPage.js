4480bc7f04b75ab970bb3b3b1aa9108e5115c4a8
Anastasia <asr@mail.ru>
2018-10-13 10:41:36 +0600

    изменение стилей

--- src/pages/components/MainPage.js
+++ src/pages/components/MainPage.js
@@ -2,10 +2,12 @@ import React from "react";
 class MainPage extends React.Component {
     render() {
         return (
-            <div>
+            <div class="color">
                 <h1> Главная страница</h1>
                 <div>Содержание</div>
+                <p>О нас</p>
             </div>
+            
         )
 
     }
