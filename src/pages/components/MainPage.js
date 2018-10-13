ef69c70f6b493c571a1e367e064f1416b944b51e
Anastasia <asr@mail.ru>
2018-10-13 10:06:21 +0600

    Hello

---
+++ src/pages/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Главная страница</h1>
+                <div>Содержание</div>
+            </div>
+        )
+
+    }
+}
+export default MainPage
